ddabe708c31cf71293ebe49624bcf7f7d61972fc
Kristian Reed <krr@equinor.com>
2018-07-16 13:14:38 +0200

    include visualsations/plotly

---
+++ visualisations/plotly/webviz_plotly/__init__.py
@@ -0,0 +1,36 @@
+import jinja2
+from os import path
+from webviz import JSONPageElement
+
+env = jinja2.Environment(
+    loader=jinja2.PackageLoader('webviz_plotly', 'templates'),
+    trim_blocks=True,
+    lstrip_blocks=True,
+    undefined=jinja2.StrictUndefined
+)
+
+
+class Plotly(JSONPageElement):
+    """
+    Plotly page element. See https://plot.ly/javascript/ for
+    usage.
+    """
+    def __init__(self, data, layout={}, config={}):
+        super(Plotly, self).__init__()
+        self['data'] = data
+        self['config'] = config
+        self['layout'] = layout
+
+    def get_template(self):
+        return env.get_template('plotly.html')
+
+    def get_js_dep(self):
+        """ Overrides :py:meth:`webportal.PageElement.get_js_dep`."""
+        deps = super(Plotly, self).get_js_dep()
+        plotly_js = path.join(
+            path.dirname(__file__),
+            'resources',
+            'js',
+            'plotly.js')
+        deps.append(plotly_js)
+        return deps

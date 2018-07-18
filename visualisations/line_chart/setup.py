978b802dad08304a5d20d0be3b0f974b4532e533
Eivind Jahren <eivind.jahren@webstep.no>
2018-07-18 10:48:28 +0200

    Initial implementation of linechart

--- visualisations/line_chart/setup.py
+++ visualisations/line_chart/setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='webviz_plotly',
+    name='webviz_line_chart',
     version='0.1.0',
     packages=find_packages("."),
     package_dir={"": "."},
     package_data={
-        'webviz_plotly': [
-            'templates/*',
-            'resources/js/*'
-        ]},
+        'webviz_line_chart': []},
     test_suite="setup.discover_test_suite",
-    install_requires=['jinja2', 'webviz'],
+    install_requires=['jinja2', 'webviz', 'webviz_plotly', 'pandas'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'mock', 'pycodestyle', 'selenium'],
     entry_points={
---
+++ visualisations/line_chart/webviz_line_chart/__init__.py
@@ -0,0 +1,34 @@
+from webviz_plotly import Plotly
+import pandas as pd
+
+
+class LineChart(Plotly):
+    """
+    Line chart page element.
+    :param data: Either a file path to a csv file or a pandas dataframe.  Each
+        column of the dataframe becomes one line in the chart. Similarly for
+        the csv file, but a special column 'index' will be used as the
+        horizontal value.
+    """
+    def __init__(self, data):
+        if isinstance(data, str):
+            self.data = pd.read_csv(data)
+            if 'index' in self.data.columns:
+                self.data.set_index(
+                        self.data['index'],
+                        inplace=True)
+                del self.data['index']
+        else:
+            self.data = data
+
+        y = self.data.index.tolist()
+
+        lines = [{
+            'x': self.data[column].tolist(),
+            'y': y,
+            'type': 'scatter',
+            'name': column
+            }
+            for column in self.data.columns]
+
+        super(LineChart, self).__init__(lines)

9ce1aa4c60524e17922a42d28e1f4c00a7314c08
Eivind Jahren <eivind.jahren@webstep.no>
2018-07-23 15:08:47 +0200

    Simple scatter plot page element.

---
+++ visualisations/scatter_plot/examples/site_example/index.md
@@ -0,0 +1,1 @@
+{{ page_element('scatter_plot', './test.csv') }}
---
+++ visualisations/scatter_plot/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webviz_scatter_plot',
+    version='0.1.0',
+    packages=find_packages("."),
+    package_dir={"": "."},
+    package_data={
+        'webviz_scatter_plot': []},
+    test_suite="setup.discover_test_suite",
+    install_requires=['jinja2', 'webviz', 'webviz_plotly', 'pandas'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'mock', 'pycodestyle', 'selenium'],
+    entry_points={
+        'webviz_page_elements': [
+            'scatter_plot = webviz_scatter_plot:ScatterPlot'
+        ]
+    },
+    zip_safe=False
+)

5650697b42b2eb46c7a4b7d35d8dbf484ec63e12
Eivind Jahren <eivind.jahren@webstep.no>
2018-07-23 14:41:10 +0200

    Added simple interface for plotly bar charts (#8)

---
+++ visualisations/bar_chart/examples/bar_chart_example.py
@@ -0,0 +1,19 @@
+from webviz import Webviz, Page
+
+from webviz_bar_chart import BarChart
+
+import pandas as pd
+
+web = Webviz('Bar Chart Example')
+
+page = Page('Bar Chart')
+
+bars1 = [10, 15, 13, 17],
+
+bars2 = [16, 5, 11, 9]
+
+bars = pd.DataFrame([bars1, bars2])
+
+page.add_content(BarChart(bars))
+web.add(page)
+web.write_html("./webviz_example", overwrite=True, display=False)

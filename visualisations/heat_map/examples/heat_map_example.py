39e5226b8752debc76bdab7367dd8266c82d474f
Eivind Jahren <eivind.jahren@webstep.no>
2018-07-24 12:22:12 +0200

    Simple heat map page element.

---
+++ visualisations/heat_map/examples/heat_map_example.py
@@ -0,0 +1,19 @@
+from webviz import Webviz, Page
+
+from webviz_heat_map import HeatMap
+
+import pandas as pd
+
+web = Webviz('Heat Map Example')
+
+page = Page('Heat Map')
+
+lines = pd.DataFrame(
+        [[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+        columns=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        index=['Morning', 'Afternoon', 'Evening'],
+        )
+
+page.add_content(HeatMap(lines))
+web.add(page)
+web.write_html("./webviz_example", overwrite=True, display=False)
---
+++ visualisations/heat_map/webviz_heat_map/__init__.py
@@ -0,0 +1,28 @@
+from webviz_plotly import Plotly
+import pandas as pd
+
+
+class HeatMap(Plotly):
+    """
+    Line chart page element.
+    :param data: Either a file path to a csv file or a pandas dataframe.  Each
+        column of the dataframe becomes one line in the chart. Similarly for
+        the csv file, but a special column 'index' will be used as the
+        horizontal value.
+    """
+    def __init__(self, data):
+        if isinstance(data, str):
+            self.data = pd.read_csv(data)
+            if 'index' in self.data.columns:
+                self.data.set_index(
+                        self.data['index'],
+                        inplace=True)
+                del self.data['index']
+        else:
+            self.data = data
+
+        super(HeatMap, self).__init__([{
+            'z': self.data.to_dict(orient='list').values(),
+            'x': list(self.data.index),
+            'y': list(self.data.columns),
+            'type': 'heatmap'}])

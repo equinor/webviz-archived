896bd7df191c4cbf14c31f396550dd5d15ec0c80
Kristian Reed <krr@equinor.com>
2018-08-02 16:38:27 +0200

    fix error in line and bar chart examples

--- visualisations/bar_chart/examples/bar_chart_example.py
+++ visualisations/bar_chart/examples/bar_chart_example.py
@@ -8,11 +8,11 @@ web = Webviz('Bar Chart Example')
 
 page = Page('Bar Chart')
 
-bars1 = [10, 15, 13, 17],
+bars1 = [10, 15, 13, 17]
 
 bars2 = [16, 5, 11, 9]
 
-bars = pd.DataFrame([bars1, bars2])
+bars = pd.DataFrame({'bars1':bars1, 'bars2':bars2})
 
 page.add_content(BarChart(bars))
 web.add(page)

e061e4911a84a7262e111fed0050615fa53c431b
Eivind Jahren <eivind.jahren@webstep.no>
2018-08-03 12:38:04 +0200

    Fixed issue with malformed classes

--- visualisations/line_chart/examples/line_chart_example.py
+++ visualisations/line_chart/examples/line_chart_example.py
@@ -8,12 +8,19 @@ web = Webviz('Line Chart Example')
 
 page = Page('Line Chart')
 
-line1 = [10, 15, 13, 17],
+line1 = [10, 15, 13, 17]
 
 line2 = [16, 5, 11, 9]
 
-lines = pd.DataFrame([line1, line2])
+lines = pd.DataFrame({
+    'line 1': line1,
+    'line 2': line2,
+    'line 3': line2,
+    'lin;;; __': line2,
+    'line 5': line2,
+    'line 6': line2,
+    })
 
-page.add_content(LineChart(lines))
+page.add_content(LineChart(lines, check_box=True))
 web.add(page)
 web.write_html("./webviz_example", overwrite=True, display=False)

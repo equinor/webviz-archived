2e05ae63c59c6fd8f9ba3eeb96af3297c1ce04f5
Eivind Jahren <eivind.jahren@webstep.no>
2018-08-08 13:45:12 +0200

    Added filtering checkboxes

---
+++ visualisations/plotly/webviz_plotly/resources/js/plotly_checkboxes.js
@@ -0,0 +1,41 @@
+function plotly_checkbox_filter(
+    containerDiv,
+    boxes,
+    filtered_data,
+    layout,
+    config
+) {
+        let keys = Object.keys(boxes);
+        function updatePlot() {
+            let enabled = {};
+            keys.forEach(key => {
+                if(!enabled[key]) enabled[key] = []
+                Object.entries(boxes[key]).forEach(([label, box]) => {
+                    if(box.checked){
+                        enabled[key].push(label);
+                    }
+                })
+            })
+            let data = filtered_data.filter(point =>
+                keys.every(key =>
+                    enabled[key].find(e => e == point.labels[key])));
+            data.forEach(point =>
+                {point.name = keys
+                        .filter(k => enabled[k] && enabled[k].length > 1)
+                        .map(key => point.labels[key])
+                        .join('-');
+                }
+            )
+            Plotly.newPlot(
+                containerDiv,
+                data,
+                layout,
+                config);
+        }
+        Object.values(boxes).forEach(y =>
+            Object.values(y).forEach(x =>
+                x.addEventListener('click',updatePlot)
+            )
+        );
+        updatePlot();
+}

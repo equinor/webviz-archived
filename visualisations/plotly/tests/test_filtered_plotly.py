2358e052b4f50ff5fbd438e9b80c0ab4aa5832dc
Eivind Jahren <eivind.jahren@webstep.no>
2018-08-08 13:45:12 +0200

    Added filtering checkboxes

---
+++ visualisations/plotly/tests/test_filtered_plotly.py
@@ -0,0 +1,58 @@
+import unittest
+import pandas as pd
+from pandas.compat import StringIO
+
+from webviz_plotly import FilteredPlotly
+
+
+class MockElement(FilteredPlotly):
+    def process_data(self, frame):
+        return [
+            {
+                'name': column,
+                'y': frame[column].tolist()
+            } for column in frame.columns]
+
+
+class TestFilteredPlotly(unittest.TestCase):
+    def setUp(self):
+        self.data = pd.DataFrame(
+            {
+                'data1': [1, 2],
+                'data2': [3, 4]
+            }
+        )
+        self.test_csv = StringIO("""
+index,data1,data2
+0,1,3
+1,2,4""")
+
+    def testFilterName(self):
+        filtered = MockElement(self.data, check_box=True)
+        self.assertTrue(
+                all('name' in data['labels']
+                    for data in filtered['data']))
+
+    def testReadCSV(self):
+        filtered = MockElement(self.data, check_box=True)
+        filtered_csv = MockElement(self.test_csv, check_box=True)
+        self.assertEqual(
+                filtered['data'],
+                filtered_csv['data']
+        )
+
+    def testFilterColumn(self):
+        filtered = MockElement(self.data, check_box_columns=['data2'])
+        self.assertTrue(
+                all('data2' in data['labels']
+                    for data in filtered['data']))
+
+    def testJsDep(self):
+        filtered = MockElement(self.data, check_box_columns=['data2'])
+        self.assertTrue(any(
+            'plotly_checkboxes.js'
+            in file for file in filtered.get_js_dep()))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- visualisations/plotly/webviz_plotly/__init__.py
+++ visualisations/plotly/webviz_plotly/__init__.py
@@ -1,6 +1,8 @@
 import jinja2
 from os import path
 from webviz import JSONPageElement
+from abc import ABCMeta, abstractmethod
+import pandas as pd
 
 env = jinja2.Environment(
     loader=jinja2.PackageLoader('webviz_plotly', 'templates'),
@@ -43,3 +45,102 @@ class Plotly(JSONPageElement):
             'plotly.js')
         deps.append(plotly_js)
         return deps
+
+
+class FilteredPlotly(Plotly):
+    """
+    Page Element for adding filtering controls to Plotly
+    plots that take a dataframe.
+
+    :param data: A dataframe that can be processed by
+       `process_data`.
+    :param check_box_columns: Columns in the dataframe
+        that should be interpreted as labels to be filtered
+        on by check boxes.
+    :param check_box: True if all traces should
+        have a corresponding check box, including or
+        excluding that trace.
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(
+            self,
+            data,
+            check_box_columns=[],
+            check_box=False,
+            **kwargs):
+        if isinstance(data, pd.DataFrame):
+            self.data = data.copy()
+        else:
+            self.data = pd.read_csv(data)
+            if 'index' in self.data.columns:
+                self.data.set_index(
+                        self.data['index'],
+                        inplace=True)
+                del self.data['index']
+
+        filtered_data = []
+        self.labels = {}
+        if len(check_box_columns):
+            grouped = self.data.groupby(check_box_columns)
+            self.labels = {}
+            for names, group in grouped:
+                if len(check_box_columns) == 1:
+                    names = [names]
+                processed = self.process_data(
+                    group.drop(check_box_columns, axis=1)
+                )
+                for point in processed:
+                    point['labels'] = {}
+                    for key, label in zip(check_box_columns, names):
+                        point['labels'][key] = label
+                        if key not in self.labels:
+                            self.labels[key] = []
+                        if label not in self.labels[key]:
+                            self.labels[key].append(label)
+                filtered_data.extend(processed)
+        else:
+                processed = self.process_data(self.data)
+                for data in processed:
+                    data['labels'] = {}
+                filtered_data.extend(processed)
+
+        self.check_box_filters = check_box_columns[:]
+        if check_box:
+            self.labels['name'] = []
+            self.check_box_filters.append('name')
+            for point in filtered_data:
+                if point['name'] not in self.labels['name']:
+                    self.labels['name'].append(point['name'])
+                point['labels']['name'] = point['name']
+
+        if not hasattr(self, 'layout'):
+            self.layout = {}
+        if not hasattr(self, 'config'):
+            self.config = {}
+        super(FilteredPlotly, self).__init__(
+                filtered_data,
+                self.layout,
+                self.config)
+        self['labels'] = self.labels
+
+    def get_js_dep(self):
+        """Extends :py:meth:webviz.PageElement.get_js_dep"""
+        deps = super(FilteredPlotly, self).get_js_dep()
+        plotly_js = path.join(
+            path.dirname(__file__),
+            'resources',
+            'js',
+            'plotly_checkboxes.js')
+        deps.append(plotly_js)
+        return deps
+
+    def get_template(self):
+        """
+        overrides :py:meth:`webviz.PageElement.get_template`.
+        """
+        return env.get_template('filtered_plotly.html')
+
+    @abstractmethod
+    def process_data(self, frame):
+        pass

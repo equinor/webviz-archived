9811ca037ecc156d89c31e1b2c1f6364d58287cf
kristofferund <djdifee@gmail.com>
2018-08-30 14:22:30 +0200

    Fixed failing tests

--- core/tests/test_site_example.py
+++ core/tests/test_site_example.py
@@ -78,6 +78,7 @@ class TestSiteExample(unittest.TestCase):
 
     def test_contains_js(self):
         includes = self.driver.find_elements_by_css_selector('script')
+        print(includes)
         filepaths1 = [tag.get_attribute('src') for tag in includes]
         link = self.driver.find_element_by_css_selector('li > a')
         link.click()

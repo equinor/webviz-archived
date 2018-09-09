78472c7bf1930064614118a35f3a575f1aca1e12
Anders Fredrik Kiær <akia@statoil.com>
2018-09-09 13:35:11 +0200

    Fix html str bug

--- core/webviz/_json_page_element.py
+++ core/webviz/_json_page_element.py
@@ -9,7 +9,10 @@ from six import iteritems, itervalues
 from ._page_element import PageElement
 from builtins import str as text
 
-_json_store_init = StringIO(u'var json_store={};')
+_json_store_init = StringIO(u''.join(['if (json_store === undefined) {', 
+                                      '    var json_store={};', 
+                                      '}']))
+
 _json_store_init.name = 'json_store_init.js'
 
 
--- core/webviz/_page_element.py
+++ core/webviz/_page_element.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from uuid import uuid4
-
+from io import StringIO
 
 class PageElement:
     """
@@ -40,7 +40,13 @@ class PageElement:
         """
         return []
 
-    def __str__(self):
+    def get_html(self, include_jslib=True):
+        """
+        :param include_jslib: If `True`, include loading of JavaScript libraries
+                  this page element depends on in the `html` string. Set this as
+                  `False` if your html file already contains the libraries.
+        :returns: Returns a `html` string representing the page element.
+        """
         css_deps = self.get_css_dep()
         js_deps = self.get_js_dep()
         html = ''
@@ -51,5 +57,11 @@ class PageElement:
                       type='text/css'>
                 </link>\n""".format(css)
         for js in js_deps:
-            html += "<script src='{0}'></script>\n".format(js)
+            if isinstance(js, StringIO):
+                html += "<script>{0}</script>\n".format(js.getvalue())
+            elif include_jslib:
+                html += "<script src='{0}'></script>\n".format(js)
         return html + self.get_template().render(element=self, root_folder='.')
+
+    def __str__(self):
+        return self.get_html()

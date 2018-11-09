3596d01584dc6c25918763abc22060d5aabefeb4
kristofferund <djdifee@gmail.com>
2018-11-09 10:44:58 +0100

    Fixed python example

--- visualizations/scatter_plot/examples/scatter_plot_example.py
+++ visualizations/scatter_plot/examples/scatter_plot_example.py
@@ -6,11 +6,17 @@ web = Webviz('Scatter Plot Example')
 
 page = Page('Scatter Plot')
 
-point1 = [10, 15, 13, 17],
+index = ['2012-01-01', '2012-01-02', '2012-01-03', '2012-01-04']
+
+point1 = [10, 15, 13, 17]
 
 point2 = [16, 5, 11, 9]
 
-points = pd.DataFrame([point1, point2])
+points = pd.DataFrame({
+    'index': index,
+    'points 1': point1,
+    'points 2': point2
+})
 
 page.add_content(ScatterPlot(points))
 web.add(page)

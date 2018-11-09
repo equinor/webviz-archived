6c8acb7848ecb62f0a482b804fd1794bf1b3482a
kristofferund <djdifee@gmail.com>
2018-11-09 12:59:47 +0100

    Updated component after clarifying that the format was correct

--- visualizations/scatter_plot/examples/scatter_plot_example.py
+++ visualizations/scatter_plot/examples/scatter_plot_example.py
@@ -6,15 +6,19 @@ web = Webviz('Scatter Plot Example')
 
 page = Page('Scatter Plot')
 
+index = ['2012-01-01', '2012-01-02', '2012-01-03', '2012-01-04']
 
 point1 = [10, 15, 13, 17]
 
 point2 = [16, 5, 11, 9]
 
 points = pd.DataFrame({
+    'index': index,
     'points 1': point1,
     'points 2': point2
 })
+
+points.set_index('index', inplace=True)
 
 page.add_content(ScatterPlot(points))
 web.add(page)
